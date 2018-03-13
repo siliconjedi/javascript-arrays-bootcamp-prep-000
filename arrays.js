b45902ab8953fa557b0355bf36927a33dc0318a5
Robert D Gauthier <rgauthie@gmail.com>
2018-03-13 19:04:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,5 @@ function addElementToBeginningOfArray (array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  return [element, ...array]
+  return array.unshift(element)
 }
